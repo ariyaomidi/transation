8cd8deacb4b69582d466aaf3ba56e632e070443a
omidi <omidiariya@gmail.com>
2023-01-24 23:34:44 +0330

    add listTileWidget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,7 +3,6 @@ import 'package:searchbar_animation/searchbar_animation.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:transation/screens/add_transaction.dart';
 
-
 import '../constant.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,20 +13,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-
-
   // این چند تا get ها همگی به ورت اجباری و پیشفرض به ویجت سرچ ابار انیمیشن اضافه میشه
 
   get textEditingController => TextEditingController();
 
-  get secondaryButtonWidget => Icon(Icons.close);
+  get secondaryButtonWidget => const Icon(Icons.close);
 
-  get trailingWidget =>  Icon(Icons.search);
+  get trailingWidget => const Icon(Icons.search);
 
   get isOriginalAnimation => false;
 
-  get buttonWidget => Icon(Icons.search);
+  get buttonWidget => const Icon(Icons.search);
 
   //******************************
 
@@ -35,10 +31,10 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        floatingActionButton:
-        FloatingActionButton(
+        floatingActionButton: FloatingActionButton(
           onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>AddTransaction()));
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const AddTransaction()));
           },
           backgroundColor: kPurpleColor,
           elevation: 0,
@@ -50,21 +46,79 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: HeaderWidget(textEditingController: textEditingController, isOriginalAnimation: isOriginalAnimation, trailingWidget: trailingWidget, secondaryButtonWidget: secondaryButtonWidget, buttonWidget: buttonWidget),
+                child: HeaderWidget(
+                    textEditingController: textEditingController,
+                    isOriginalAnimation: isOriginalAnimation,
+                    trailingWidget: trailingWidget,
+                    secondaryButtonWidget: secondaryButtonWidget,
+                    buttonWidget: buttonWidget),
               ),
-              Spacer(),
-              EmptyWidget(),
-              Spacer(),
+              // Spacer(),
+              // EmptyWidget(),
+
+              ListView.builder(itemBuilder: (context, index) {
+                return const MyListTileWidget();
+              },itemCount: 10,),
+
+
+              const Spacer(),
             ],
           ),
-
         ),
       ),
     );
   }
 }
 
+// my List tile widget
+
+class MyListTileWidget extends StatelessWidget {
+  const MyListTileWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.red),
+            child: const Icon(
+              Icons.arrow_downward,
+              color: Colors.white,
+            ),
+          ),
+          const Spacer(flex: 1),
+          const Text('Gym'),
+          const Spacer(flex: 30),
+          Column(
+            children: [
+              Row(
+                children: const [
+                  Text('تومان'),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text('10,000'),
+                ],
+              ),
+              const Text('1401/02/03')
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
 //Header Widget
+
 class HeaderWidget extends StatelessWidget {
   const HeaderWidget({
     Key? key,
@@ -75,16 +129,15 @@ class HeaderWidget extends StatelessWidget {
     required this.buttonWidget,
   }) : super(key: key);
 
-  final  textEditingController;
-  final  isOriginalAnimation;
-  final  trailingWidget;
-  final  secondaryButtonWidget;
-  final  buttonWidget;
+  final textEditingController;
+  final isOriginalAnimation;
+  final trailingWidget;
+  final secondaryButtonWidget;
+  final buttonWidget;
 
   @override
   Widget build(BuildContext context) {
     return Row(
-
       children: [
         Expanded(
           child: SearchBarAnimation(
@@ -105,17 +158,19 @@ class HeaderWidget extends StatelessWidget {
   }
 }
 
-
 // Empty Widget
+
 class EmptyWidget extends StatelessWidget {
   const EmptyWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       children: [
-        SvgPicture.asset('assets/images/empty.svg',),
-        Text('تراکنشی وجود ندارد'),
+        SvgPicture.asset(
+          'assets/images/empty.svg',
+        ),
+        const Text('تراکنشی وجود ندارد'),
       ],
     );
   }
